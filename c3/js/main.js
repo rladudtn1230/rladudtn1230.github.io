073defa6479ed1a3d7b6ee7686da7475b5a06f1b
rladudtn1230 <55184084+rladudtn1230@users.noreply.github.com>
2019-11-25 16:35:03 +0900

    Add files via upload

--- c3/js/main.js
+++ c3/js/main.js
@@ -3,8 +3,14 @@ $(document).ready(function(){
         var right = $(".menu_wrap").css("right")
         if(right == "0px"){
             $(".menu_wrap").css("right","-500px")
+            $(".line1").css("transform","rotate(0deg)").css("top","0")
+            $(".line2").css("display","block").css("top","15px")
+            $(".line3").css("transform","rotate(0deg)").css("top","30px")
         }else{
             $(".menu_wrap").css("right","0")
+            $(".line1").css("transform","rotate(-45deg)").css("top","15px")
+            $(".line2").css("display","none").css("top","15px")
+            $(".line3").css("transform","rotate(45deg)").css("top","15px")
         }
     })
 })
